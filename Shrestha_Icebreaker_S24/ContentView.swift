1681a6ae78d8e4dc2609f90b5ff70773d19c09ee
dipesh <dipeshshrestha0314@gmail.com>
2024-02-03 01:56:04 -0500

    completed initial form view

--- Shrestha_Icebreaker_S24/ContentView.swift
+++ Shrestha_Icebreaker_S24/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firstName: String
+    @State var lastName: String
+    @State var prefName: String
+    @State var selectedOption = 0
+    @State var answer: String
+    
+    let options: [String] = ["What is your favourite color?","What do you like to do during your spare time?","What is your aim?"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("First Name", text: $firstName)
+                    .textFieldStyle(.roundedBorder)
+                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(2)
+                TextField("Last Name",text: $lastName)
+                    .textFieldStyle(.roundedBorder)
+                    .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(2)
+            }
+            TextField("Preferred Name", text: $prefName)
+                .textFieldStyle(.roundedBorder)
+                .border(Color.gray)
+                .cornerRadius(2)
+            Picker("Select a Question", selection: $selectedOption){
+                ForEach(0 ..< options.count){
+                    Text(self.options[$0])
+                }
+            }
+            .pickerStyle(MenuPickerStyle())
+            .padding()
+            TextField("Your answer", text: $answer)
+                .textFieldStyle(.roundedBorder)
+                .cornerRadius(2)
+                .border(Color.gray)
+                .padding(.bottom, 10)
+            Button(action: {
+                
+            }){
+                Text("Submit")
+                    .frame(width: 340)
+                    .padding(.vertical, 5)
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+        Spacer()
+        Spacer()
+        
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(firstName: "", lastName: "", prefName: "", answer: "")
 }
