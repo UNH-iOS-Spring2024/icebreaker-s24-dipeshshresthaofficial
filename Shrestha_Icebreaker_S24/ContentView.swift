469eb68fc2d8538885741c832a9a14cdaf171cb7
dipesh <dipeshshrestha0314@gmail.com>
2024-02-03 01:09:27 -0500

    Initial Commit

---
+++ Shrestha_Icebreaker_S24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shrestha_Icebreaker_S24
+//
+//  Created by Dipesh Shrestha on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shrestha_Icebreaker_S24/Shrestha_Icebreaker_S24App.swift
@@ -0,0 +1,17 @@
+//
+//  Shrestha_Icebreaker_S24App.swift
+//  Shrestha_Icebreaker_S24
+//
+//  Created by Dipesh Shrestha on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Shrestha_Icebreaker_S24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
