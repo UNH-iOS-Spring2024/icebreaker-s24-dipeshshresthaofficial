6725195d83121b1e876b752ad09531fc6c9e673a
dipesh <dipeshshrestha0314@gmail.com>
2024-02-11 11:16:16 -0500

    updated the UI style of Detail view

--- Shrestha_Icebreaker_S24/UserDataDetailView.swift
+++ Shrestha_Icebreaker_S24/UserDataDetailView.swift
@@ -10,28 +10,45 @@ import SwiftUI
 struct UserDataDetailView: View {
     var user: UserData
     var body: some View {
-        VStack{
-            HStack{
-                Text("First Name: ")
-                Text(user.firstName)
+        List{
+            Section{
+                HStack{
+                    Image(systemName: "person")
+                        .font(.title2)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundColor(Color.white)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    VStack(alignment: .leading){
+                        HStack{
+                            Text(user.firstName)
+                            Text(user.lastName)
+                        }
+                        Text("xxx@gmail.com")
+                            .tint(Color.gray)
+                    }
+                }
             }
-            HStack{
-                Text("Last Name: ")
-                Text(user.lastName)
+            
+            Section("Icebreaker Question"){
+                HStack{
+                    Image(systemName: "questionmark.circle")
+                        .font(.system(size: 22))
+                        .foregroundColor(Color.gray)
+                    Text(user.question)
+                }
             }
-            HStack{
-                Text("Preferred Name: ")
-                Text(user.prefName)
-            }
-            HStack{
-                Text("Question: ")
-                Text(user.question)
-            }
-            HStack{
-                Text("Answer: ")
-                Text(user.answer)
+            
+            Section("Response"){
+                HStack{
+                    Image(systemName: "checkmark.seal")
+                        .font(.system(size: 22))
+                        .foregroundColor(Color.gray)
+                    Text(user.answer)
+                }
             }
         }
+        Spacer()
     }
 }
 
